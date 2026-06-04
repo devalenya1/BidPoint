b7ee764d0751a1e707b810e91e29274e4dc9c224
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2026-06-04 10:52:43 +0100

    jhj

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -888,7 +888,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => const SearchPage(), // Your existing search page
+            builder: (context) => const Filter(), // Your existing search page
           ),
         );
       },
