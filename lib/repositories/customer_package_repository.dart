2c80227b6552c9433233f5fee75080ea76170449
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2026-06-24 19:40:55 +0100

    ghg

--- lib/repositories/customer_package_repository.dart
+++ lib/repositories/customer_package_repository.dart
@@ -10,6 +10,7 @@ import 'package:active_ecommerce_flutter/repositories/api-request.dart';
 import 'package:http/http.dart' as http;
 
 class CustomerPackageRepository {
+  
   Future<CustomerPackageResponse> getList() async {
     String url = ('${AppConfig.BASE_URL}/customer-packages');
 
@@ -27,23 +28,26 @@ class CustomerPackageRepository {
 
     var post_body = jsonEncode({"package_id": "${id}"});
     final response = await ApiRequest.post(
-        url: url,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${access_token.$}",
-          "App-Language": app_language.$!,
-        },
-        body: post_body,middleware: BannedUser());
-    // bool checkResult = ResponseCheck.apply(response.body);
-    //
-    // //if (!checkResult) return responseCheckModelFromJson(response.body);
-
-   print(response.body);
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ${access_token.$}",
+        "App-Language": app_language.$!,
+      },
+      body: post_body,
+      middleware: BannedUser()
+    );
+    
+    print(response.body);
     return commonResponseFromJson(response.body);
   }
 
-  Future<dynamic> offlinePackagePayment(
-      {packageId, method, trx_id, photo}) async {
+  Future<dynamic> offlinePackagePayment({
+    required packageId, 
+    required method, 
+    required trx_id, 
+    required photo
+  }) async {
     String url = ('${AppConfig.BASE_URL}/offline/packages-payment');
 
     var post_body = jsonEncode({
@@ -54,60 +58,19 @@ class CustomerPackageRepository {
     });
 
     print(post_body);
-
     print(url.toString());
+    
     final response = await ApiRequest.post(
-        url: url,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${access_token.$}",
-          "App-Language": app_language.$!,
-        },
-        body: post_body);
-    bool checkResult = ResponseCheck.apply(response.body);
-
-    if (!checkResult) return responseCheckModelFromJson(response.body);
-
-    return commonResponseFromJson(response.body);
-  }
-
-}
-
-  Future<dynamic> freePackagePayment(id) async {
-    String url = ('${AppConfig.BASE_URL}/free/packages-payment');
-
-    var post_body = jsonEncode({"package_id": "${id}"});
-    final response = await ApiRequest.post(
-        url: url,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${access_token.$}",
-          "App-Language": app_language.$!,
-        },
-        body: post_body,
-        middleware: BannedUser());
-    return commonResponseFromJson(response.body);
-  }
-
-  Future<dynamic> offlinePackagePayment(
-      {packageId, method, trx_id, photo}) async {
-    String url = ('${AppConfig.BASE_URL}/offline/packages-payment');
-
-    var post_body = jsonEncode({
-      "package_id": "${packageId}",
-      "payment_option": "${method}",
-      "trx_id": "${trx_id}",
-      "photo": "${photo}",
-    });
-    final response = await ApiRequest.post(
-        url: url,
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer ${access_token.$}",
-          "App-Language": app_language.$!,
-        },
-        body: post_body,
-        middleware: BannedUser());
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ${access_token.$}",
+        "App-Language": app_language.$!,
+      },
+      body: post_body,
+      middleware: BannedUser()
+    );
+    
     return commonResponseFromJson(response.body);
   }
 }
